64e22cb5d230ec09845c110d9f58384d37dc3dc5
pinplengpai <pinplengpai@gmail.com>
2023-05-12 17:10:21 +0200

    first time in a long time

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            It's Pin Plengpai
           </p>
         </a>
 
@@ -70,7 +70,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Pinplengpai
           </p>
         </a>
 
