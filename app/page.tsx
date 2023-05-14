8ca95e41980e88c3fe76674e336959543a896138
pinplengpai <pinplengpai@gmail.com>
2023-05-14 15:48:35 +0200

    w

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1> HI HI it's PIN change one more time</h1>
+      <h1>Hi</h1>
       
     </main>
   )
